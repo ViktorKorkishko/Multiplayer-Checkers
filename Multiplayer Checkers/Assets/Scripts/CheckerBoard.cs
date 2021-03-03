f88686e17f45769d8ca941e333566964c2578c0b
ViktorKorkishko <korkishko.viktor2001@gmail.com>
2021-03-03 21:25:08 +0200

    Moving checkers

--- Multiplayer Checkers/Assets/Scripts/CheckerBoard.cs
+++ Multiplayer Checkers/Assets/Scripts/CheckerBoard.cs
@@ -6,16 +6,20 @@ public class CheckerBoard : MonoBehaviour
 {
     [Header("Board params")] 
     public Checker[,] Board;
-    
+
     [SerializeField] private int _boardSize;
     [SerializeField] private Vector3 _boardOffset;
     [SerializeField] private Vector3 _checkerOffset;
-
-
+    
     [Header("Spawning params")] 
     [SerializeField] private GameObject _whiteCheckerPrefab;
     [SerializeField] private GameObject _blackCheckerPrefab;
 
+    [Header("Input params")] 
+    [SerializeField] private Checker _selectedChecker;
+    [SerializeField] private Vector2Int _mouseOver;
+    [SerializeField] private Vector2Int _startDrag;
+    [SerializeField] private Vector2Int _endDrag;
 
     private void Awake()
     {
@@ -25,6 +29,70 @@ public class CheckerBoard : MonoBehaviour
     private void Start()
     {
         GenerateBoard();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UpdateMouseOver();
+            
+            SelectChecker(_mouseOver);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            UpdateMouseOver();
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            UpdateMouseOver();
+            TryMove(_startDrag, _mouseOver);
+        }
+    }
+
+    private void UpdateMouseOver()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25f,
+            LayerMask.GetMask("Board")))
+        {
+            _mouseOver = new Vector2Int((int) (hit.point.x - _boardOffset.x), (int) (hit.point.z - _boardOffset.z));
+        }
+        else
+        {
+            _mouseOver = new Vector2Int(-1, -1);
+        }
+    }
+
+    private void SelectChecker(Vector2Int position)
+    {
+        // if out of bounds
+        if (position.x <= 0 || position.x >= _boardSize || 
+            position.y <= 0 || position.y >= _boardSize)
+        {
+            return;
+        }
+
+        Checker checker = Board[position.x, position.y];
+        if (checker)
+        {
+            _selectedChecker = checker;
+            _startDrag = _mouseOver;
+        }
+    }
+
+    private void TryMove(Vector2Int startPosition, Vector2Int endPosition)
+    {
+        // multiplayer support
+        _startDrag = startPosition;
+        _endDrag = endPosition;
+
+        _selectedChecker = Board[startPosition.x, startPosition.y];
+        
+        MoveChecker(_selectedChecker, endPosition);
     }
 
     private void GenerateBoard()
